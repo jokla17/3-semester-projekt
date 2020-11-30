a40e475f1e669dcadff804f7522b8eedb434ed56
Wahid Tobias <54397547+wtrwinberg@users.noreply.github.com>
2020-11-30 15:55:32 +0100

    Optimal speed from physical machine test.

--- kajate/src/WebClient/scripts/speedOptimizer.js
+++ kajate/src/WebClient/scripts/speedOptimizer.js
@@ -2,27 +2,27 @@ let speedOptimizer = () => {
     let beerType = document.getElementById("slProductType").value;
     switch (beerType) {
         case "0":
-            document.getElementById("tfMachineSpeed").value = 450;
+            document.getElementById("tfMachineSpeed").value = 435;
             break;
 
         case "1":
-            document.getElementById("tfMachineSpeed").value = 225;
+            document.getElementById("tfMachineSpeed").value = 50;
             break;
 
         case "2":
-            document.getElementById("tfMachineSpeed").value = 115;
+            document.getElementById("tfMachineSpeed").value = 85;
             break;
 
         case "3":
-            document.getElementById("tfMachineSpeed").value = 150;
+            document.getElementById("tfMachineSpeed").value = 275;
             break;
 
         case "4":
-            document.getElementById("tfMachineSpeed").value = 75;
+            document.getElementById("tfMachineSpeed").value = 65;
             break;
 
         case "5":
-            document.getElementById("tfMachineSpeed").value = 95;
+            document.getElementById("tfMachineSpeed").value = 50;
             break;
     }
 }
